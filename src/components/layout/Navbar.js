2ffc189f277d89f2510ebc4d7cd410865a9495c9
Anuj8957 <anujjaiswalcse0020@gmail.com>
2021-04-22 18:41:13 +0530

    View part added

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -21,6 +21,7 @@ const Navbar = () => {
                    
                     </ul>
                 </div>
+                <button className="btn btn-outline-light ">Add User</button>
             </div>
             </div>
         </nav>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from "react-router-dom";
 
- const Home = () => {
+const Home = () => {
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        loadUsers();
+    }, []);
+
+    const loadUsers = async () => {
+        const result = await axios.get('http://localhost:3003/users');
+        setUser(result.data);
+    }
+
     return (
         <div className="container">
             <div className="py-4">
-                <h1>Home</h1>
+                <table class="table  border shadow">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">Sno.</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">UserName</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            user.map((user,index) =>(
+                                <tr>
+                                    <th scope="row">{index + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.email}</td>
+                                    <td>
+                                        <Link className="btn btn-sm btn-primary mx-2">View</Link>
+                                        <Link className="btn btn-sm btn-outline-primary mx-2">Edit</Link>
+                                        <Link className="btn btn-sm btn-danger mx-2">Delete</Link>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
             </div>
         </div>
     );
